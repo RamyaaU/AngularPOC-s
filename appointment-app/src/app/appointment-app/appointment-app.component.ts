0e770ab5743f0e0ea5effad6f354a5f8131cbe56
RamyaU <ramyayadav2016@gmail.com>
2024-07-04 16:06:45 +0530

    create app component

---
+++ appointment-app/src/app/appointment-app/appointment-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointment-app.component.html',
+  styleUrl: './appointment-app.component.css'
+})
+export class AppointmentAppComponent {
+
+}
