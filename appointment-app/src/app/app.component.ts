034399ac5484ca6524460d27c853fb9226a825c5
RamyaU <ramyayadav2016@gmail.com>
2024-07-04 19:19:06 +0530

    created appointment interface and handled 2 way data binding

--- appointment-app/src/app/app.component.ts
+++ appointment-app/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppointmentAppComponent } from './appointment-app/appointment-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, AppointmentAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'appointment-app';
 }
--- appointment-app/src/app/appointment-app/appointment-app.component.ts
+++ appointment-app/src/app/appointment-app/appointment-app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-app',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './appointment-app.component.html',
   styleUrl: './appointment-app.component.css'
 })
 export class AppointmentAppComponent {
 
+  newAppointmentTitle : string = "";
+  newAppointmentDate : Date = new Date();
+
+  addAppointment(){
+    alert(this.newAppointmentTitle + " "  + this.newAppointmentDate);
+  }
+  
 }
