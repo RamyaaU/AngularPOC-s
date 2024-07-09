e7537a522ca0aee3f84a00873e5abee32d62b0ff
RamyaU <ramyayadav2016@gmail.com>
2024-07-09 15:32:40 +0530

    implementation of 2 way data binding

--- book-management-system/src/app/app.component.ts
+++ book-management-system/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookComponent } from './book/book.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
--- book-management-system/src/app/book/book.component.ts
+++ book-management-system/src/app/book/book.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { Book } from '../models/book.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
 export class BookComponent {
 
-  book : Book = {
-    id: 1,
-    title : "Revolution",
-    author : "Chetan Bhagat"
+  // book : Book = {
+  //   id: 1,
+  //   title : "Revolution",
+  //   author : "Chetan Bhagat"
+  // }
+
+  title: string = '';
+  author: string = '';
+
+  addBook(): void {
+    
+    console.log(`Title: ${this.title}, Author: ${this.author}`);
+
+
+    this.title = '';
+    this.author = '';
   }
 }
