b665dc92704b0622c06e324b24656ff60f644310
RamyaU <ramyayadav2016@gmail.com>
2024-07-10 18:02:30 +0530

    added routes

--- event-booking-app/src/app/app.routes.ts
+++ event-booking-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventReservationFormComponent } from './event-reservation-form/event-reservation-form.component';
+import { EventReservationListComponent } from './event-reservation-list/event-reservation-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"form", component:EventReservationFormComponent},
+    {path:"list", component:EventReservationListComponent},
+];
