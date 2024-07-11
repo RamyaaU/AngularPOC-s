739f9844705a240949c3f29dbc8bf1db631475b0
RamyaU <ramyayadav2016@gmail.com>
2024-07-11 18:31:34 +0530

    created sample navigation

--- event-booking-app/src/app/app.component.ts
+++ event-booking-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventReservationFormComponent } from './event-reservation-form/event-reservation-form.component';
+import { EventReservationModule } from './event-reservation/event-reservation.module';
+import { HomeModule } from './home/home.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventReservationModule, HomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
