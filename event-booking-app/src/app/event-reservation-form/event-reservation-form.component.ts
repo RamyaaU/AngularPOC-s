588280a66c61774974d770fcf6a82756f20704f7
RamyaU <ramyayadav2016@gmail.com>
2024-07-22 13:42:45 +0530

    created teh form component

--- event-booking-app/src/app/event-reservation-form/event-reservation-form.component.ts
+++ event-booking-app/src/app/event-reservation-form/event-reservation-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-event-reservation-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './event-reservation-form.component.html',
   styleUrl: './event-reservation-form.component.css'
 })
 export class EventReservationFormComponent {
 
+  eventBookingForm: FormGroup = new FormGroup({});
 }
--- event-booking-app/src/app/event-reservation/event-reservation.module.ts
+++ event-booking-app/src/app/event-reservation/event-reservation.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventReservationFormComponent } from '../event-reservation-form/event-reservation-form.component';
 import { EventReservationListComponent } from '../event-reservation-list/event-reservation-list.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     EventReservationFormComponent,
-    EventReservationListComponent
+    EventReservationListComponent,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
+
 export class EventReservationModule { }
