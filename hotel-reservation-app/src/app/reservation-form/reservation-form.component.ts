da1e3f2c2b9d9b1030fc3142aa245032e651dd60
RamyaU <ramyayadav2016@gmail.com>
2024-07-22 13:42:56 +0530

    creating a form group

--- hotel-reservation-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-reservation-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
 export class ReservationFormComponent {
 
+    reservationForm: FormGroup = new FormGroup({});
 }
--- hotel-reservation-app/src/app/reservation/reservation.module.ts
+++ hotel-reservation-app/src/app/reservation/reservation.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ReservationFormComponent,
-    ReservationListComponent
+    ReservationListComponent,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
